64d2d80d8d7430367ff4174b0816b349b5403894
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2025-03-12 01:19:47 +0200

    Add link and hooks for navigation

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -1,15 +1,42 @@
-import React from "react";
+// import React from "react";
+// import Header from "../../../components/header";
+
+// export interface PageProps {
+//   params: { id: string };
+// }
+
+// export default function Page({ params }: PageProps) {
+//   return (
+//     <>
+//       <Header>Company ({params.id})</Header>
+//     </>
+//   );
+// }
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Header from "../../../components/header";
 
 export interface PageProps {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }
 
 export default function Page({ params }: PageProps) {
+  const [id, setId] = useState<string | null>(null);
+
+  useEffect(() => {
+    params.then(data => {
+      setId(data.id);
+    });
+  }, [params]);
+
+  if (id === null) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
-      <Header>Company ({params.id})</Header>
+      <Header>Company ({id})</Header>
     </>
   );
 }
-
