1a147316fa5e14f65b970f7e0b94dc7f8a0c75fd
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2025-03-13 02:05:23 +0200

    Add error handling components

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -1,21 +1,37 @@
-// import React from "react";
-// import Header from "../../../components/header";
+// "use client";
+
+// import React, { useEffect, useState } from "react";
+// import Header from "@/app/components/header";
 
 // export interface PageProps {
-//   params: { id: string };
+//   params: Promise<{ id: string }>;
 // }
 
 // export default function Page({ params }: PageProps) {
+//   const [id, setId] = useState<string | null>(null);
+  
+//   useEffect(() => {
+//     params.then(data => {
+//       setId(data.id);
+//     });
+//   }, [params]);
+
+//   if (id === null) {
+//     return <div>Loading...</div>;
+//   }
+
 //   return (
 //     <>
-//       <Header>Company ({params.id})</Header>
+//       <Header>Company ({id})</Header>
 //     </>
 //   );
 // }
+
 "use client";
 
 import React, { useEffect, useState } from "react";
 import Header from "@/app/components/header";
+import { notFound } from "next/navigation";
 
 export interface PageProps {
   params: Promise<{ id: string }>;
@@ -25,18 +41,27 @@ export default function Page({ params }: PageProps) {
   const [id, setId] = useState<string | null>(null);
 
   useEffect(() => {
-    params.then(data => {
-      setId(data.id);
-    });
+    params
+      .then(data => {
+        setId(data.id);
+      })
+      .catch(() => {
+        notFound();
+      });
   }, [params]);
 
-  if (id === null) {
-    return <div>Loading...</div>;
-  }
+  useEffect(() => {
+    if (id !== null) {
+      const idNumber = Number.parseInt(id, 10);
+      if (Number.isNaN(idNumber)) {
+        notFound();
+      }
+    }
+  }, [id]);
 
   return (
     <>
-      <Header>Company ({id})</Header>
+      <Header>Company ({id ?? "Loading..."})</Header>
     </>
   );
 }
