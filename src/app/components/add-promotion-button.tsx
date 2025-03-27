5bbcc835c93cf27618c22e291322a552029fb47b
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2025-03-27 03:07:24 +0200

    Add using react-query

---
+++ src/app/components/add-promotion-button.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React from "react";
+import Button from '@/app/components/button';
+
+export interface AddPromotionButtonProps {
+    companyId: string;
+}
+
+export default function AddPromotionButton({
+    companyId,
+}: AddPromotionButtonProps) {
+    const router = useRouter();
+    return (
+        <Button
+            onClick={() => router.push(`/companies/${companyId}/new-promotion`)}
+        >
+            Add promotion
+        </Button>
+    );
+}
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -44,7 +44,7 @@ export interface SummaryStats {
    avatar?: string;
  }
  
- export interface Promotion {
+ export interface PromotionType {
    id: string;
    title: string;
    description: string;
@@ -99,7 +99,7 @@ export const getPromotions = async (
   params: Record<string, string> = {},
   init?: RequestInit,
 ) => {
-  return sendRequest<Promotion[]>(
+  return sendRequest<PromotionType[]>(
     `${buildUrl('promotions')}?${stringifyQueryParams(params)}`,
     init,
   );
